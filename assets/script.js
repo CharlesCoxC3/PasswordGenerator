5fe7343e266438c252108af78f44ac5c26dcca58
Charles Cox III <charlescoxc3@Charless-Mac-mini.local>
2022-10-01 14:27:26 -0700

    changed code, moved if statements, added for loop, added mathRandom generator

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
   console.log("you clicked the button");
-
+  
   var userChoice = window.prompt("What legnth do you want you password to be? ( 8-128 characters)")
 
   var passwordLength = parseInt(userChoice)
-
 
   if (!userChoice) {
     return;
@@ -15,29 +14,61 @@ function generatePassword(){
   
   if (isNaN(passwordLength)){
     window.alert("That is not an accepted value..")
+    return;
   } 
   else if (passwordLength < 8 || passwordLength > 128){
     window.alert("That is not an accepeted value..")
+    return;
   }
   else {
     window.alert("The number " + passwordLength + " was accepted")
-  }
+  } 
 
-  var lowerCase = window.confirm("The password must contain lowercase letters, do you wish to continue?")
-    if (lowerCase){ var upperCase = window.confirm("The password must contain uppercase letters, do you wish to continue?")
-      if (upperCase){var specialChar = window.confirm("The password must contain numeric/special characters, do you wish to continue?")}
-    }
-  else {
-    return;
-  };
+  var confirm1 = window.confirm("The password may contain lowercase letters, do you wish to continue?");
+  var confirm2 = window.confirm("The password may contain uppercase letters, do you wish to continue?");
+  var confirm3 = window.confirm("The password may contain numbers, do you wish to continue?");
+  var confirm4 = window.confirm("The password may contain special characters, do you wish to continue?");
+  
 
-  var passwordCriteria = {
-    lowerCaseList: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    numberList: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    symbolsList: ["!", "@", "#", "$", "%", "^", "&", "*"],
-  };
+  var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  var upperCaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var passwordOptions = [];
+      
+      if (confirm1 === true) {
+        var passwordOptions = passwordOptions.concat(lowerCaseList)
+      }
+      if (confirm2 === true) {
+        var passwordOptions = passwordOptions.concat(upperCaseList)
+      }
+      if (confirm3 === true) {
+        var passwordOptions = passwordOptions.concat(numberList)
+      }
+      if (confirm4 === true) {
+        var passwordOptions = passwordOptions.concat(symbolsList)
+      }
 
-}
+      console.log(passwordOptions)
+
+  var generatedPassword = ""
+  
+    for (var i = 0; i < passwordLength; i++){
+      var result = passwordOptions[Math.floor(Math.random()*passwordOptions.length)];
+      console.log(result)
+      generatedPassword += result
+    };
+
+    if(confirm1, confirm2, confirm3, confirm4 !== true){
+      return "No selections were made"
+    };
+
+return generatedPassword
+}; 
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -50,3 +81,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
